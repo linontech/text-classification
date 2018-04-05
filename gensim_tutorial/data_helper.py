d958a7a84bc6155673b3f6b9dcb300a5369651c1
William <fivejthree@outlook.com>
2018-04-05 10:04:41 +0800

    shuffle needs attention
    critical, wrong order samples to train logistic regression due to shuffle samples

--- gensim_tutorial/data_helper.py
+++ gensim_tutorial/data_helper.py
@@ -6,31 +6,46 @@ from collections import namedtuple
 from itertools import tee
 import jieba
 import numpy as np
+import os
 
 from utils import accept_sentence
 
 jieba.load_userdict('../dataset/digital forum comments/digital_selfdict.txt')
-Document = namedtuple('Document', 'words tags label')
+Document = namedtuple('Document', 'words tags split label')
 
 """
     read text data
 """
-def load_text_label(text_path):
+def load_text_label(text_path, classes_num, portion, shuffle=True):
     """
-    scan through the texts one time, and return how many texts each class has.
-    one class per this function.
+    classes_num: dict of num of each class
+    split: portion [train_portion, dev_portion, ...]
     p.s you should know how many texts each classes got.
     p.s you should shuffle texts of each classes first, due to farly splitting dataset.
     """
-    count = 0
-    with open(text_path, encoding='utf-8') as texts:
-        for line_no, line in enumerate(texts):
-            text, label = line.split('\t')
-            words = jieba.lcut(text, HMM=False)
-            tags = [line_no]
-            count += 1
-            yield Document(words, tags, label)
+    train_lens, dev_lens, train_len_total, dev_len_total = {}, {}, 0, 0
+    for _class, num in classes_num.items():
+        train_lens[_class] = int(num * portion[0])
+        dev_lens[_class] = num - train_lens[_class]
+        train_len_total += train_lens[_class]
+        dev_len_total += dev_lens[_class]
 
+    documents=[]
+    files = os.listdir(text_path)
+    for file in files:  # filename=labelname
+        classes_name = file.split('.')[0]
+        with open(text_path+file, encoding='utf-8') as texts:
+            for line_no, line in enumerate(texts):
+                text, label = line.split('\t')
+                text = accept_sentence(text)
+                words = jieba.lcut(''.join(text), HMM=False)
+                tags = [line_no]
+                label = 1 if label.rstrip()=='负面' else 0
+                split = 'train' if line_no<train_lens[classes_name] else 'dev'
+                documents.append(Document(words, tags, split, label))  # yield Document(words, tags, label)
+    assert train_len_total==len([d for d in documents if d.split=='train']), 'train text load error!'
+    if shuffle: random.shuffle(documents)
+    return documents, train_len_total, dev_len_total
 
 def load_data_without_store_ixs_labeled(filepath, data_size=350000):
     """
@@ -38,8 +53,8 @@ def load_data_without_store_ixs_labeled(filepath, data_size=350000):
     read a large text file with random line access
     """
     # random_sampler for big file which does not fit in memory
-    # generate random numer to sample data, size=350000 due to limit of machine
     # random_nums = set(random.sample(range(44001245), data_size))
+    # if you want to generate len of samples which equal the len of text, this method just fails.
     with open(filepath, 'rb') as f:
         f.seek(0, 2)
         filesize = f.tell()
@@ -50,6 +65,7 @@ def load_data_without_store_ixs_labeled(filepath, data_size=350000):
             f.readline()  # Skip current line (because we might be in the middle of a line)
             line = f.readline().rstrip()
             line = line.decode('utf-8')
+
             text, label = line.split('\t')
             text = accept_sentence(text)
             # print (' '.join(text))
@@ -59,10 +75,6 @@ def load_data_without_store_ixs_labeled(filepath, data_size=350000):
             label = 1 if label=='负面' else 0
             yield Document(words, tag, int(label))
 
-
-def getVecs(model, corpus_size, vec_size):
-    vecs = [np.array(model.docvecs[z].reshape(1, vec_size)) for z in range(corpus_size)]
-    return np.concatenate(vecs)
 
 def copy_gen(_gen):
     """
--- gensim_tutorial/doc2vec_main.py
+++ gensim_tutorial/doc2vec_main.py
@@ -7,9 +7,9 @@
 
 import datetime
 import multiprocessing
-import os
 from collections import OrderedDict
 from collections import defaultdict
+from random import shuffle
 
 import jieba
 import numpy as np
@@ -17,81 +17,78 @@ from gensim.models.doc2vec import Doc2Vec
 from sklearn import linear_model
 from sklearn.metrics import classification_report
 
-from gensim_t.data_helper import getVecs, load_text_label, load_data_without_store_ixs_labeled
+from gensim_t.data_helper import load_text_label
 from gensim_t.train_utils import elapsed_timer
 
 jieba.load_userdict('../dataset/digital forum comments/digital_selfdict.txt')
 # logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s', level=logging.INFO)
 cores = multiprocessing.cpu_count()
 
-corpus_path = '../dataset/digital forum comments/split_20180403/all.txt'
-corpus = load_text_label(corpus_path)
+corpus_path = '../dataset/digital forum comments/split_20180403/classes/'
+# path = '../dataset/digital forum comments/split_20180403/classes/'
+classes_num = {'0': 84145, '1': 22057}  # get these nums first
+split = [0.8, 0.2]
+corpus, train_len_total, dev_len_total = load_text_label(corpus_path, classes_num, split, shuffle=True)
+corpus_train = [d for d in corpus if d.split == 'train']
+corpus_dev = [d for d in corpus if d.split == 'dev']
+# corpus_train_label = np.array([d.label for d in corpus if d.split == 'train'])
+
+corpus_len = len(corpus)
 
 simple_models = [  # 参数调整，模型比较
     # PV-DM w/ concatenation - window=5 (both sides) approximates paper's 10-word total window size
-    Doc2Vec(dm=1, dm_concat=1, size=100, window=10, negative=20, hs=0, min_count=2, workers=cores),
+    Doc2Vec(dm=1, dm_concat=1, size=100, window=10, negative=20, hs=0, min_count=2, workers=cores, compute_loss=True),
     # PV-DBOW
-    Doc2Vec(dm=0, size=200, negative=20, hs=0, min_count=2, workers=cores),
+    Doc2Vec(dm=0, size=100, negative=20, hs=0, min_count=2, workers=cores, compute_loss=True),
     # PV-DM w/ average
-    Doc2Vec(dm=1, dm_mean=1, size=100, window=10, negative=20, hs=0, min_count=2, workers=cores),
+    Doc2Vec(dm=1, dm_mean=1, size=100, window=10, negative=20, hs=0, min_count=2, workers=cores, compute_loss=True),
 ]
 models_by_name = OrderedDict((str(model), model) for model in simple_models)
 
-# Speed up setup by sharing results of the 1st model's vocabulary scan
+# 只要初始化一次词库
 simple_models[0].build_vocab(corpus)  # PV-DM w/ concat requires one special NULL word so it serves as template
 print(simple_models[0])
 for model in simple_models[1:]:
     model.reset_from(simple_models[0])
     print(model)
 
-path = '../dataset/digital forum comments/split_20180403/classes/'
-files = os.listdir(path)
-classes_num = {'0': 84145, '1': 22057}  # get these nums first
-split = [0.8, 0.2]
-train_lens, dev_lens, train_len_total, dev_len_total = {}, {}, 0, 0
-for _class, num in classes_num.items():
-    train_lens[_class] = int(num * split[0])
-    dev_lens[_class] = num - train_lens[_class]
-    train_len_total += train_lens[_class]
-    dev_len_total += dev_lens[_class]
-
 best_error = defaultdict(lambda: 1.0)  # To selectively print only best errors achieved
-alpha, min_alpha, epoches = 0.1, 0.001, 50
+alpha, min_alpha, epoches = 2.5, 0.1, 20
 alpha_delta = (alpha - min_alpha) / epoches
 print("START %s" % datetime.datetime.now())
 infer_steps = 20
 infer_alpha = 0.1
 for epoch in range(1, epoches + 1):
-    text_gens = {}
-    for file in files:  # generate train, dev text sets' generators in shuffled order
-        # print(file)  # check you know the generators' order; filename=labelname
-        class_name=file.split('.')[0]
-        text_gens[class_name] = load_data_without_store_ixs_labeled(path + file, classes_num[class_name])
-
-    train_gen = [next(text_gens[c]) for c in classes_num.keys() for _ in range(train_lens[c])]
-    dev_gen = [next(text_gens[c]) for c in classes_num.keys() for _ in range(dev_lens[c])]
-
+    shuffle(corpus)
     for name, train_model in models_by_name.items():
-        # Train
         duration = 'na'
         train_model.alpha, train_model.min_alpha = alpha, alpha
         with elapsed_timer() as elapsed:
-            train_model.train(train_gen, total_examples=train_len_total, epochs=1)
+            train_model.train(corpus, total_examples=corpus_len, epochs=1)  # Train
             duration = '%.1f' % elapsed()
-        print('-%d epoch， duration=%s' % (epoch, duration))
+        print('-%d epoch, duration=%s,' % (epoch, duration))
 
         if (epoch % 5 == 0) or (epoch == epoches):
+            dev_labels = [doc.label for doc in corpus if doc.split == 'dev']
             clf = linear_model.SGDClassifier(max_iter=3000, verbose=1, tol=20, learning_rate='constant', eta0=0.01,
                                              class_weight={1: 1, 0: 1})
 
-            clf.fit(getVecs(train_model, train_len_total, train_model.vector_size),
-                    np.array([i.label for i in train_gen]))
+            clf.fit([train_model.docvecs[doc.tags[0]] for doc in corpus if doc.split == 'train'],
+                    [doc.label for doc in corpus if doc.split == 'train'])
 
-            classifedLabels = clf.predict(np.array(
-                [train_model.infer_vector(doc.words, steps=infer_steps, alpha=infer_alpha) for doc in dev_gen]))
+            classifedLabels_dev = clf.predict(
+                [train_model.docvecs[doc.tags[0]] for doc in corpus if doc.split == 'dev'])
             print(
-                '[clf  ' + name + ']' + '\n' + str(classification_report([i.label for i in dev_gen], classifedLabels)))
+                ' [clf  ' + name + ' ]' + '\n' + str(classification_report(dev_labels, classifedLabels_dev)))
 
-        alpha -= alpha_delta
+            classifedLabels_dev_infer = clf.predict(np.array(
+                [train_model.infer_vector(doc.words, steps=infer_steps, alpha=infer_alpha) for doc in corpus if
+                 doc.split == 'dev']))
+            print(
+                ' [clf  ' + name + ' infer ]' + '\n' + str(
+                    classification_report(dev_labels, classifedLabels_dev_infer)))
 
-print("END %s" % str(datetime.datetime.now()))
+            alpha -= alpha_delta
+            print('[complete epoch %d ]' % (epoch))
+
+            print("END %s" % str(datetime.datetime.now()))
